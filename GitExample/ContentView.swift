e2d3a1f7cc35d00ba72e055b43a4972b422a5f99
Elliot Ginzburg <s019637@students.lmsd.org>
2021-03-11 12:11:46 -0500

    initial commit

---
+++ GitExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitExample
+//
+//  Created by Elliot Ginzburg (student LM) on 3/11/21.
+//  Copyright © 2021 Elliot Ginzburg (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
